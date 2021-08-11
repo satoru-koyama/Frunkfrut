1b49e972e290b4f0ca0fe8a85dffc11a032c3d9d
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-08-11 06:52:32 +0000

    [Update] adminのログインページ、ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-  sessions:      'admin/admins/sessions',
-  passwords:     'admin/admins/passwords',
-  registrations: 'admin/admins/registrations'
-}
-  devise_for :users, controllers: {
-  sessions:      'public/users/sessions',
-  passwords:     'public/users/passwords',
-  registrations: 'public/users/registrations'
-}
 
   namespace :admin do
+
+    devise_for :admins, controllers: {
+      sessions:      'admin/admins/sessions',
+      passwords:     'admin/admins/passwords',
+      registrations: 'admin/admins/registrations'
+    }
+
     get "/", to: "homes#top", as: "top"
+
     resources "posts", only: [:show]
+
     resources "comments", only: [:show]
+
     resources "users", only: [:index, :show] do
       member do
         get "block"
@@ -22,11 +22,21 @@ Rails.application.routes.draw do
         patch "undo"
       end
     end
+
   end
 
   scope module: :public do
+
+    devise_for :users, controllers: {
+      sessions:      'public/users/sessions',
+      passwords:     'public/users/passwords',
+      registrations: 'public/users/registrations'
+    }
+
     root "homes#top", as: "top"
+
     get "/about", to: "homes#about", as: "about"
+
     resources "posts", only: [:index, :show, :create, :update] do
       member do
         patch "delete"
@@ -39,6 +49,7 @@ Rails.application.routes.draw do
       resource "post_favorites", only: [:create, :destroy]
       resource "post_reports", only: [:create, :destroy]
     end
+
     resources "comments", only: [] do
       resources "replies", only: [:create, :update] do
         member do
@@ -48,10 +59,12 @@ Rails.application.routes.draw do
       resource "comment_favorites", only: [:create, :destroy]
       resource "comment_reports", only: [:create, :destroy]
     end
+
     resources "replies", only: [] do
       resource "reply_favorites", only: [:create, :destroy]
       resource "reply_reports", only: [:create, :destroy]
     end
+
     resources "users", only: [:show, :update] do
       collection do
         get "block"
@@ -62,6 +75,7 @@ Rails.application.routes.draw do
       resource "follow_relationships", only: [:create, :destroy]
       resource "block_relationships", only: [:create, :destroy]
     end
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
